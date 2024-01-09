776398e7d537bcaf3cbefde91666d0dd159cec32
Coworking Metz <contact@coworking-metz.fr>
2024-01-09 13:52:32 +0100

    export + GET

--- wp-content/mu-plugins/coworking-app/app-settings.php
+++ wp-content/mu-plugins/coworking-app/app-settings.php
@@ -3,7 +3,7 @@ add_action(
     'rest_api_init',
     function () {
         register_rest_route('cowo/v1', '/app-settings', array(
-            'methods'  => 'GET',
+            'methods'  => ['GET','POST'],
             'callback' => function ($request) {
 
                 coworking_app_check($request);
