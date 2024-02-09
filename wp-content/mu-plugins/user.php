38646e80797e21d830e9f60510724e0d5d2ddc86
Coworking Metz <contact@coworking-metz.fr>
2024-02-09 10:18:16 +0100

    dates de visites hors des creneaux

--- wp-content/mu-plugins/user.php
+++ wp-content/mu-plugins/user.php
@@ -12,3 +12,36 @@ add_action('profile_update', function ($user_id, $old_user_data) {
         error_log('Error calling webhook: ' . $response->get_error_message());
     }
 }, 99, 2);
+
+
+
+add_filter( 'user_row_actions', function( $actions, $user ) {
+    $actions['manager'] = '<a target="_blank" href="' . esc_url( 'https://manager.coworking-metz.fr/members/'.$user->ID ) . '">Fiche dans manager</a>';
+    return $actions;
+}, 10, 2 );
+
+
+/**
+ * Finaliser un compte
+ */
+add_action('admin_bar_menu', function ($admin_bar) {
+    if (!is_admin()) return;
+    $screen = get_current_screen();
+    if ($screen->base != 'user-edit') return;
+    $user_id = $_GET['user_id'] ?? false;
+    if (!$user_id) return;
+    $user_info = get_userdata($user_id);
+    $roles = $user_info->roles;
+    if (in_array('subscriber', $roles) || in_array('bookmify-customer', $roles)) return;
+
+    
+
+    $admin_bar->add_menu(array(
+        'id'    => 'manager',
+        'title' => 'Voir la fiche dans manager',
+        'href'  => esc_url( 'https://manager.coworking-metz.fr/members/'.$user_id ),
+        'meta'  => array(
+            'target' => '_blank'
+        ),
+    ));
+}, 100);
