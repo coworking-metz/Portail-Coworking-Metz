be78107b5e499d9c995e102a5b70c40bd8e804b2
Coworking Metz <contact@coworking-metz.fr>
2024-03-07 18:01:30 +0100

    purge ag

--- wp-content/mu-plugins/includes/ag.inc.php
+++ wp-content/mu-plugins/includes/ag.inc.php
@@ -1,3 +1,28 @@
 <?php
 
 include __DIR__ . '/ag/main.inc.php';
+
+
+if (isset($_GET['purge-votes-ca'])) {
+    add_action('admin_init', function () {
+        $date = ag_date();
+        $keys = ['a-vote-' . $date, 'votes-' . $date];
+
+        // Récupération de tous les utilisateurs
+        $users = get_users();
+        foreach ($users as $user) {
+            foreach ($keys as $key) {
+                // Vérification de l'existence de la métadonnée
+                if ($value = get_user_meta($user->ID, $key, true)) {
+                    // Renommage de la métadonnée
+                    update_user_meta($user->ID, '_trash-' . $key, $value);
+                    // Suppression de la métadonnée originale
+                    delete_user_meta($user->ID, $key);
+                }
+            }
+        }
+        wp_redirect($_SERVER['HTTP_REFERER']);
+        exit;
+    });
+
+}
