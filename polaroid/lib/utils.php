e85253626ab0bd86a0955eccc226066766f8afce
Coworking Metz <contact@coworking-metz.fr>
2024-03-08 16:03:46 +0100

    cache pola

--- polaroid/lib/utils.php
+++ polaroid/lib/utils.php
@@ -9,8 +9,10 @@
  * 
  * @param string $imagePath Path to the image file.
  * @param int|null $width Optional width to resize the image.
+ * @param string $destinationPath Path where the  image will be written.
  */
-function outputImageWithHeaders($imagePath, $width = null) {
+function outputImageWithHeaders($imagePath, $width = null, $destinationPath = false)
+{
     if (!file_exists($imagePath)) {
         header('HTTP/1.0 404 Not Found');
         echo 'File not found.';
@@ -34,19 +36,60 @@ function outputImageWithHeaders($imagePath, $width = null) {
     }
     if ($extension === 'png') {
         header('Content-Type: image/jpeg');
-        imagejpeg($image);
+        write_and_output_image($image, $destinationPath, 'jpg');
     } else {
         $mimeType = mime_content_type($imagePath);
         header("Content-Type: $mimeType");
         if ($mimeType === 'image/jpeg') {
-            imagejpeg($image);
+            write_and_output_image($image, $destinationPath, 'jpg');
         } elseif ($mimeType === 'image/gif') {
-            imagegif($image);
+            write_and_output_image($image, $destinationPath, 'gif');
         } elseif ($mimeType === 'image/png') {
-            imagepng($image);
+            write_and_output_image($image, $destinationPath, 'png');
         }
     }
 
     imagedestroy($image);
     exit;
+}
+
+
+function write_and_output_image($image, $path, $type)
+{
+    if ($type == 'jpg') {
+        imagejpeg($image);
+        return imagejpeg($image, $path);
+    }
+    if ($type == 'gif') {
+        imagegif($image);
+        return imagegif($image, $path);
+    }
+    if ($type == 'png') {
+        imagepng($image);
+        return imagepng($image, $path);
+    }
+}
+
+/**
+ * Affiche le contenu d'une image si elle existe, est plus récente que l'âge spécifié et termine l'exécution du script.
+ * 
+ * @param string $imagePath Chemin vers l'image.
+ * @param int $age Âge maximum de l'image en secondes. Par défaut, un mois.
+ * @return mixed
+ */
+function outputImageIfExists($imagePath, $age = 2592000)
+{
+
+    if (file_exists($imagePath)) {
+        $fileAge = time() - filemtime($imagePath);
+        if ($fileAge > $age) {
+            return unlink($imagePath);
+        }
+        $mime_type = mime_content_type($imagePath);
+        CF::cacheHeaders($age);
+        header('Content-Type: '.$mime_type);
+
+        readfile($imagePath);
+        exit();
+    }
 }
