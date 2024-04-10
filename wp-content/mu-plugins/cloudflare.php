c43a9a095dc7f7e00f39a70a74e1719504b2ed94
Coworking Metz <contact@coworking-metz.fr>
2024-04-10 21:57:51 +0200

    cf

--- wp-content/mu-plugins/cloudflare.php
+++ wp-content/mu-plugins/cloudflare.php
@@ -2,5 +2,5 @@
 
 
 add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_script('cf.js', 'https://cloudflare.coworking-metz.fr/cf.js', array(), false, true);
+    wp_enqueue_script('cf.js', 'https://cloudflare.coworking-metz.fr/cf.js?nocache', array(), false, true);
 });
