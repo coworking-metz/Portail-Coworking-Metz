c2b26deabbb0f238dd4e952cf8b206588f33ccf2
Coworking Metz <contact@coworking-metz.fr>
2024-04-23 09:42:35 +0200

    commandes + events

--- events/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('[href="#set-nb"]').addEventListener('click', e => {
-        e.preventDefault();
-
-        document.body.classList.toggle('set-nb');
-    });
-})
--- events/ping.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include __DIR__ . '/inc/main.inc.php';
-
-$evenements = getEvenements();
-
-if ($evenements) echo 'true';
