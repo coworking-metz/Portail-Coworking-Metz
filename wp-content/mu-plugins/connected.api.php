c7833921bced89b441dbd13e270053ea3a068474
Coworking Metz <contact@coworking-metz.fr>
2024-04-24 21:48:27 +0200

    dark magenta

--- wp-content/mu-plugins/connected.api.php
+++ wp-content/mu-plugins/connected.api.php
@@ -1,9 +1,9 @@
 <?php
 
-if(isset($_GET['is-connected'])) {
-    add_action('init', function() {
+if (isset($_GET['is-connected'])) {
+    add_action('init', function () {
         CoworkingMetz\CloudFlare::noCacheHeaders();
-        if($uid = get_current_user_id()) {
+        if ($uid = get_current_user_id()) {
             $data = get_userdata($uid);
             $user = $data->data;
             $user->roles = $data->roles;
@@ -14,12 +14,20 @@ if(isset($_GET['is-connected'])) {
 
             $user->firstName = $parts[0];
             $user->lastName = implode(' ', array_slice($parts, 1));
-            $user->_first_order_date= date_francais(get_user_meta($user->ID, '_first_order_date', true));
+            $user->_first_order_date = date_francais(get_user_meta($user->ID, '_first_order_date', true));
             $user->hash = sha1($user->ID . AUTH_KEY);
         } else {
-            $user = ['hash'=>sha1(session_id() . AUTH_KEY)];
+
+            $hash = $_COOKIE['session-hash'] ?? false;
+            $cache = true;
+            if (!$hash) {
+                $cache = false;
+                $hash = sha1(time() . AUTH_KEY);
+                setcookie('session-hash', $hash);
+            }
+            $user = ['hash' => $hash, 'cache'=>$cache];
         }
-        $payload = ['user'=>$user];
+        $payload = ['user' => $user];
         header('Content-type: application/json');
         echo json_encode($payload);
         exit;
