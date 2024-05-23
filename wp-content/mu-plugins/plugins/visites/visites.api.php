7236e53b421229b93e496ebca71b124823dc4efb
Coworking Metz <contact@coworking-metz.fr>
2024-05-23 17:27:48 +0200

    api visites

---
+++ wp-content/mu-plugins/plugins/visites/visites.api.php
@@ -0,0 +1,26 @@
+<?php
+
+add_action('rest_api_init', function () {
+    register_rest_route('cowo/v1', '/visites', array(
+        'methods' => 'GET',
+        'permission_callback' => function () {
+            return true || current_user_can('administrator');
+        },
+        'callback' => function ($data) {
+            $users = fetch_users_with_future_visite();
+            $payload = [];
+            foreach($users as $user) {
+                $data = [];
+                $data['wpUserId']=$user->ID;
+                $data['email']=$user->user_email;
+                $data['name']=$user->display_name;
+                $data['visite']=date('Y-m-d', strtotime($user->visite));
+                $data['activite']=$user->activite;
+                $payload[]=$data;
+            }
+            return $payload;
+        }
+    ));
+    
+});
+
