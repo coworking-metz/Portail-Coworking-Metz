3dfff4c740bb7f31b99b34c273f9f92d0ba40e91
Coworking Metz <contact@coworking-metz.fr>
2024-05-23 17:28:01 +0200

    Restriction de l'api des viistes

--- wp-content/mu-plugins/plugins/visites/visites.api.php
+++ wp-content/mu-plugins/plugins/visites/visites.api.php
@@ -4,7 +4,7 @@ add_action('rest_api_init', function () {
     register_rest_route('cowo/v1', '/visites', array(
         'methods' => 'GET',
         'permission_callback' => function () {
-            return true || current_user_can('administrator');
+            return current_user_can('administrator');
         },
         'callback' => function ($data) {
             $users = fetch_users_with_future_visite();
