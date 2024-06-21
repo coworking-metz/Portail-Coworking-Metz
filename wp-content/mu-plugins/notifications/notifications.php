c4b05fe584be21a45ac2020cfc0d708f9e062e2f
Coworking Metz <contact@coworking-metz.fr>
2024-06-21 12:18:22 +0200

    notif dans url

--- wp-content/mu-plugins/notifications/notifications.php
+++ wp-content/mu-plugins/notifications/notifications.php
@@ -11,28 +11,43 @@ add_action('init', function () {
 });
 
 
-
+if ($_GET['notification'] ?? false) {
+    add_action('wp_footer', function () {
+        $data = json_decode(stripslashes($_GET['notification']), true);
+        $data['time'] = time();
+        echo generateNotification($data);
+    });
+}
 function generateNotification($data)
 {
 
+    $GLOBALS['notification'] = $GLOBALS['notification'] ?? 0;
 
-	$GLOBALS['notification'] = $GLOBALS['notification']??0;
-
-	if($GLOBALS['notification']) return;
-	$GLOBALS['notification']++;
+    if ($GLOBALS['notification']) return;
+    $GLOBALS['notification']++;
     $id = sha1(json_encode($data));
     $cta = '';
-    if ($data['cta']) {
+    if ($data['cta'] ?? false) {
         $cta = '<span class="cta"><a href="' . $data['cta']['url'] . '" class="button">' . $data['cta']['caption'] . '</a></span>';
     }
-    return '<div class="notification" role="alert" data-id="'.$id.'" data-type="'.($data['type']??'default').'" data-once="'.(($data['once']??false) ? 'true':'false').'">
+    if ($data['temporaire'] ?? false) {
+        $data['duree'] = 5;
+    }
+    return '<div class="notification" role="alert" data-id="' . $id . '" data-type="' . ($data['type'] ?? 'default') . '" data-once="' . (($data['once'] ?? false) ? 'true' : 'false') . '" data-duration="' . ($data['duree'] ?? false) . '">
     <div>
     <div>
-    <figure><img src="' . $data['image'] . '"></figure>
+    <figure><img src="' . ($data['image'] ?? '') . '"></figure>
     <p><b class="titre">' . $data['titre'] . '</b><span>' . $data['texte'] . '</span></p>
     </div>
     ' . $cta . '
     </div>
     <button>&#x2716;</button>
   </div>';
+}
+
+
+function wp_redirect_notification($uri, $notification)
+{
+    wp_redirect($uri . '?notification=' . urlencode(json_encode($notification)));
+    exit;
 }
