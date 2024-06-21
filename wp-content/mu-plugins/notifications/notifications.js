85331c60d1506371c740b5e6abf1087ae95baa30
Coworking Metz <contact@coworking-metz.fr>
2024-06-21 12:18:22 +0200

    notif dans url

--- wp-content/mu-plugins/notifications/notifications.js
+++ wp-content/mu-plugins/notifications/notifications.js
@@ -2,13 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let notifications = document.querySelectorAll('.notification:not([data-visible])');
 
+    removeQueryVar('notification');
     notifications.forEach(notification => {
         const id = notification.dataset.id;
         const key = 'hide-notification-' + id
         if (sessionStorage.getItem(key)) {
+
             delete notification.dataset.visible
         } else {
-            if(notification.dataset.once) {
+            if (notification.dataset.once) {
                 sessionStorage.setItem(key, true);
             }
             notification.dataset.visible = true;
@@ -16,6 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 delete notification.dataset.visible
                 sessionStorage.setItem(key, true);
             })
+            if (notification.dataset.duration) {
+                setTimeout(() =>
+                    delete notification.dataset.visible
+                    , notification.dataset.duration * 1000)
+            }
         }
     });
+    /**
+ * Supprimer le paramètre de notification de l'URL sans recharger la page
+ */
+    function removeQueryVar(name) {
+        const url = new URL(window.location.href);
+        const searchParams = url.searchParams;
+
+        // Vérifier si le paramètre de notification existe
+        if (searchParams.has(name)) {
+            // Supprimer le paramètre de notification
+            searchParams.delete(name);
+
+            // Mettre à jour l'URL sans recharger la page
+            const newUrl = url.origin + url.pathname + (searchParams.toString() ? '?' + searchParams.toString() : '');
+            window.history.replaceState({}, document.title, newUrl);
+        }
+    }
 })
