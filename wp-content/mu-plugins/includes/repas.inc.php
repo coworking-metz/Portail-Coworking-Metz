e73f700eb3291e21d8d775251f4f5d4c84ebb5ce
Coworking Metz <contact@coworking-metz.fr>
2024-09-13 17:12:35 +0200

    fonctions repas améliorées

--- wp-content/mu-plugins/includes/repas.inc.php
+++ wp-content/mu-plugins/includes/repas.inc.php
@@ -3,12 +3,26 @@
 
 function get_meals($product)
 {
-    return intval(get_field('coupons_repas', $product['product_id'] ?? $product->get_id() ?? false));
+    $pid = false;
+    if (is_array($product)) {
+        $pid = $product['product_id'] ?? $product['ID'] ?? intval($product);
+    } else if (is_object($product)) {
+        $pid = $product->get_id();
+    }
+    if (!$pid) return;
+    return intval(get_field('coupons_repas', $pid));
 }
 
 
 
 function get_meal_price($product)
 {
-    return get_field('prix_repas', $product['product_id'] ?? $product->get_id() ?? false);
+    $pid = false;
+    if (is_array($product)) {
+        $pid = $product['product_id'] ?? $product['ID'] ?? intval($product);
+    } else if (is_object($product)) {
+        $pid = $product->get_id();
+    }
+    if (!$pid) return;
+    return get_field('prix_repas', $pid);
 }
