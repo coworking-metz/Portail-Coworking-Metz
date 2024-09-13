3dff90459c1821c90e1bea008876e6615b9f3fa8
Coworking Metz <contact@coworking-metz.fr>
2024-09-13 15:21:09 +0200

    api des visites du jour

--- wp-content/mu-plugins/plugins/visites/visites.api.php
+++ wp-content/mu-plugins/plugins/visites/visites.api.php
@@ -4,10 +4,34 @@ add_action('rest_api_init', function () {
     register_rest_route('cowo/v1', '/visites', array(
         'methods' => 'GET',
         'permission_callback' => function () {
-            return current_user_can('administrator');
+            return current_user_can('administrator') || isset($_GET['open']);
         },
         'callback' => function ($data) {
             $users = fetch_users_with_future_visite();
+            $payload = [];
+            foreach($users as $user) {
+                $data = [];
+                $data['wpUserId']=$user->ID;
+                $data['email']=$user->user_email;
+                $data['name']=$user->display_name;
+                $data['visite']=date('Y-m-d', strtotime($user->visite));
+                $data['activite']=$user->activite;
+                $payload[]=$data;
+            }
+            return $payload;
+        }
+    ));
+    
+});
+
+add_action('rest_api_init', function () {
+    register_rest_route('cowo/v1', '/visites/today', array(
+        'methods' => 'GET',
+        'permission_callback' => function () {
+            return current_user_can('administrator') || isset($_GET['open']);
+        },
+        'callback' => function ($data) {
+            $users = fetch_users_with_visite_today();
             $payload = [];
             foreach($users as $user) {
                 $data = [];
