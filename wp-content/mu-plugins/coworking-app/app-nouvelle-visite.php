22ba47175dadf5c73c816b45cec728d760e592b2
Gilles FRANCOIS <gilles@sopress.net>
2024-12-03 13:14:23 +0100

    gestion des nomades

--- wp-content/mu-plugins/coworking-app/app-nouvelle-visite.php
+++ wp-content/mu-plugins/coworking-app/app-nouvelle-visite.php
@@ -18,33 +18,11 @@ add_action('rest_api_init', function () {
         'callback' => function ($request) {
             coworking_app_check($request);
 
-            // // Allow from any origin
-            // if (isset($_SERVER['HTTP_ORIGIN'])) {
-            //     // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-            //     // you want to allow, and if so:
-            //     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-            //     header('Access-Control-Allow-Credentials: true');
-            //     header('Access-Control-Max-Age: 86400');    // cache for 1 day
-            // }
-
-            // // Access-Control headers are received during OPTIONS requests
-            // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-            //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            //         // may also be using PUT, PATCH, HEAD etc
-            //         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-            //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            //         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-            //     exit(0);
-            // }
-
 
 
             $payload = $_GET['payload'] ?? false;
             if (!$payload) return;
-            
+
             $params = json_decode(stripslashes($payload), true);
 
             $user = $params['user'] ?? false;
@@ -54,25 +32,32 @@ add_action('rest_api_init', function () {
 
             $nom = $user['prenom'] . ' ' . $user['nom'] . ' (' . $user['email'] . ')';
             $activite = $user['activite'];
-            if($activite) {
-                $nom.=' - '.$activite;
+            if ($activite) {
+                $nom .= ' - ' . $activite;
             }
-            $date = new DateTime($params['visite'], new DateTimeZone('Europe/Paris'));
-            $start = $date->format(DateTime::RFC3339);
+            if ($user['nomade']) {
+                $user['role'] = 'customer';
+                $user_id = create_wp_user_if_not_exists($user, ['nomade' => true, 'activite' => $activite]);
+                if ($user_id) {
+                    set_transient('auto_login_' . $user_id, true, ONE_DAY);
+                    return rest_ensure_response(['user_id' => $user_id]);
+                }
+            } else {
+                $date = new DateTime($params['visite'], new DateTimeZone('Europe/Paris'));
+                $start = $date->format(DateTime::RFC3339);
 
-            $date->modify('+30 minutes');
-            $end = $date->format(DateTime::RFC3339);
+                $date->modify('+30 minutes');
+                $end = $date->format(DateTime::RFC3339);
 
-            $event = ['name' => 'Visite pour ' . $nom, 'start' => $start, 'end' => $end];
+                $user_id = create_wp_user_if_not_exists($user, ['visite' => date_maline($start), 'activite' => $activite]);
+                if ($user_id) {
+                    $event = ['name' => 'Visite pour ' . $nom, 'start' => $start, 'end' => $end];
 
-            $user_id = create_wp_user_if_not_exists($user, ['visite' => date_maline($start), 'activite'=>$activite]);
-            if ($user_id) {
-                // addEventToCalendar($user_id, $event);
+                    envoyerMailAlerte($user_id, ['activite' => $activite ? $activite : 'Non renseigné']);
+                    envoyerMailVisite($user_id, $params['visite']);
 
-                envoyerMailAlerte($user_id, ['activite'=>$activite?$activite:'Non renseigné']);
-                envoyerMailVisite($user_id, $params['visite']);
-
-                return rest_ensure_response(['event' => $event]);
+                    return rest_ensure_response(['event' => $event, 'user_id' => $user_id]);
+                }
             }
         },
     ]);
