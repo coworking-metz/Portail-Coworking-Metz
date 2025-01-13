ca8d7cbabe553615c24bedca102a83a5248c570e
Coworking Metz <contact@coworking-metz.fr>
2025-01-13 12:23:11 +0100

    oauth

--- wp-content/plugins/oauth2-provider/includes/admin/page-server-options.php
+++ wp-content/plugins/oauth2-provider/includes/admin/page-server-options.php
@@ -190,7 +190,7 @@ function wo_server_options_page() {
                                     <td>
                                         <input type="number" name="wo_options[refresh_token_lifetime]"
                                                value=""
-                                               placeholder="86400" disabled/>
+                                               placeholder="2592000" disabled/>
                                         <p class="description">
                                             How long a refresh token is valid (seconds) - Leave blank for default (24
                                             hours)
