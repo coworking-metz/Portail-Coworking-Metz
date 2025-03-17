30636610bbc2021e2ef07c464788d95df4101f2f
Gilles FRANCOIS <gilles@sopress.net>
2025-03-17 14:50:20 +0100

    envoyerMailAlerteNomade et envoyerMailNomade avec DEFAULT_TO_EMAIL

--- wp-content/mu-plugins/plugins/email_templates.php
+++ wp-content/mu-plugins/plugins/email_templates.php
@@ -3,27 +3,6 @@
 /**
  * Remplir les champs de choix des templates dans la page des reglages des visites avec la liste des templates d'emails
  */
-/*add_action('init', function () {
-
-    $templates = brevo_getTemplates();
-    
-    foreach (['email_recap_visite', 'email_alerte_avent','email_alerte_cowo', 'email_confirmation_de_visite', 'email_finalisation_compte'] as $nom_champ) {
-        add_filter('acf/load_field/name=' . $nom_champ, function ($field) use ($templates) {
-
-            $field['choices'] = [''];
-                foreach ($templates as $template) {
-                    $field['choices'][$template['id']] = '#' . $template['id'] . ' - ' . $template['name'];
-                }
-
-            return $field;
-        });
-    }
-});
-*/
-
-/**
- * Remplir les champs de choix des templates dans la page des reglages des visites avec la liste des templates d'emails
- */
 add_action('init', function () {
     $args = array(
         'post_type' => 'viwec_template',
@@ -31,11 +10,11 @@ add_action('init', function () {
         'posts_per_page' => -1,
     );
     $posts = get_posts($args);
-    $templates = brevo_getTemplates();
-
-    foreach (['email_recap_visite', 'email_rappel_visite', 'email_alerte_avent','email_alerte_cowo', 'email_confirmation_de_visite', 'email_finalisation_compte'] as $nom_champ) {
-        add_filter('acf/load_field/name=' . $nom_champ, function ($field) use ($posts, $templates) {
-
+    
+    foreach (['email_recap_visite', 'email_rappel_visite', 'email_alerte_avent','email_alerte_cowo','email_alerte_cowo_nomade', 'email_confirmation_de_visite', 'email_confirmation_nomade', 'email_finalisation_compte'] as $nom_champ) {
+        add_filter('acf/load_field/name=' . $nom_champ, function ($field) use ($posts) {
+            $templates = brevo_getTemplates();
+            
             $field['choices'] = [''];
             foreach ($templates as $template) {
                 $field['choices']['brevo-'.$template['id']] = 'Brevo - #' . str_pad($template['id'],3,0,STR_PAD_LEFT) . ' - ' . $template['name'];
