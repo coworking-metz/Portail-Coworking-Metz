59a82fc5b13ea1a1a1aa9ff59de4e9d039b9eb80
Coworking Metz <contact@coworking-metz.fr>
2025-03-28 10:02:49 +0100

    redirection vrs le protocole de l'appli poulailler

--- wp-content/mu-plugins/plugins/compte/logout.php
+++ wp-content/mu-plugins/plugins/compte/logout.php
@@ -6,8 +6,11 @@ if (isset($_GET['logout'])) {
         // Déconnecte l'utilisateur courant
         wp_logout();
 
+		$protocols = wp_allowed_protocols();
+		$protocols[]= 'poulailler';
         // Redirige vers l'URL spécifiée dans 'redirect_to' ou vers la page d'accueil
-        $redirect_to = isset($_GET['redirect_to']) ? esc_url($_GET['redirect_to']) : home_url('/');
+        $redirect_to = isset($_GET['redirect_to']) ? esc_url($_GET['redirect_to'],$protocols) : home_url('/');
+
         wp_redirect($redirect_to);
         exit;
     });
